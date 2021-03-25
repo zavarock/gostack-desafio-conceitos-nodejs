06124fa64ac703644ea787f918d8cece6b916968
Junior <junior.coh@live.com>
2021-03-25 14:55:17 -0300

    TODO completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const {v4: uuid, validate: isUuid} = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,76 @@ app.use(cors());
 
 const repositories = [];
 
+function checkRepositoryId(request, response, next) {
+    const {id} = request.params;
+    if (!isUuid(id)) {
+        return response.status(400).json({
+            message: 'Invalid repository ID.'
+        });
+    }
+
+    const repositoryIndex = repositories.findIndex(repository => id === repository.id);
+    if (repositoryIndex < 0) {
+        return response.status(404).json({
+            message: 'Repository not found.'
+        });
+    }
+
+    // In Express.js, response.locals is used when we need to pass custom variables to the next middleware.
+    response.locals.repositoryIndex = repositoryIndex;
+
+    return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const {title, url, techs} = request.body;
+    const newRepository = {
+        id: uuid(),
+        title,
+        url,
+        techs,
+        likes: 0
+    };
+
+    repositories.push(newRepository);
+
+    return response.status(201).json(newRepository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", checkRepositoryId, (request, response) => {
+    const repositoryIndex = response.locals.repositoryIndex;
+    const repository = repositories[repositoryIndex];
+
+    const {title, url, techs} = request.body;
+
+    repository.title = title;
+    repository.url = url;
+    repository.techs = techs;
+
+    repositories[repositoryIndex] = repository;
+
+    return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", checkRepositoryId, (request, response) => {
+    repositories.splice(response.locals.repositoryIndex, 1);
+
+    return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", checkRepositoryId, (request, response) => {
+    const repositoryIndex = response.locals.repositoryIndex;
+    const repository = repositories[repositoryIndex];
+
+    repository.likes += 1;
+
+    repositories[repositoryIndex] = repository;
+
+    return response.status(201).json(repository);
 });
 
 module.exports = app;
